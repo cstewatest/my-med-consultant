483507bd1276500b4bf7a1c7ce8b77e34dc0c6e1
Christina Stewart <christina.v.stewart@gmail.com>
2018-03-19 13:34:17 +0900

    add and run prettier on files except app js

--- src/constants/FormStyles.js
+++ src/constants/FormStyles.js
@@ -1,29 +1,28 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
-export default styles = StyleSheet.create({
+export default (styles = StyleSheet.create({
   prompt: {
-    fontFamily: 'open-sans-bold',
+    fontFamily: "open-sans-bold",
     paddingBottom: 10,
-    color: '#ffffff',
+    color: "#ffffff",
     fontSize: 20
   },
   container: {
     padding: 20
   },
   buttonText: {
-    alignSelf: 'center',
-    fontFamily: 'open-sans-bold'
+    alignSelf: "center",
+    fontFamily: "open-sans-bold"
   },
-  button: { 
-    marginTop: 12, 
+  button: {
+    marginTop: 12,
     height: 40,
-    backgroundColor: '#ffffff',
-    borderColor: '#ffffff',
+    backgroundColor: "#ffffff",
+    borderColor: "#ffffff",
     borderRadius: 4,
-    alignSelf: 'center',
+    alignSelf: "center",
     width: 80,
-    borderWidth: .5,
-    justifyContent: 'center'
+    borderWidth: 0.5,
+    justifyContent: "center"
   }
-})
-
+}));
